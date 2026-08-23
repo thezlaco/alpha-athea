5eaa86bc0827e85ae2eb27d9d1060201efe55c94
thezlaco <thezlaco@users.noreply.github.com>
2026-08-24 00:41:52 +0200

    Missing fillMaxWidth import

--- app/src/main/java/com/athea/app/ui/main/MainScreen.kt
+++ app/src/main/java/com/athea/app/ui/main/MainScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
