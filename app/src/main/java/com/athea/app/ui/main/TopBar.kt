2d491df60f3db8ba15a03c99bd38814d09c7ccef
thezlaco <thezlaco@users.noreply.github.com>
2026-08-25 01:17:07 +0200

    Add widthIn import

--- app/src/main/java/com/athea/app/ui/main/TopBar.kt
+++ app/src/main/java/com/athea/app/ui/main/TopBar.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
