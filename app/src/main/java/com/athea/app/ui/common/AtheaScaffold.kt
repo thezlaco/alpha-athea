13aeba0471c58f1e50c20628bbf984d0d54bfe69
thezlaco <thezlaco@users.noreply.github.com>
2026-09-08 23:03:12 +0200

    Fix AtheaScaffold missing fillMaxWidth import

--- app/src/main/java/com/athea/app/ui/common/AtheaScaffold.kt
+++ app/src/main/java/com/athea/app/ui/common/AtheaScaffold.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
